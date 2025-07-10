d5f6f9acf6efc6de0668665238b4e6e97a6dabb2
nanda <nandagolla074@gmail.com>
2025-07-10 16:36:32 +0530

    terraform module vpc modifications

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,11 @@ output "vpc_id" {
 output "public_subnet_list" {
   value = aws_subnet.public[*].id
 }
+
+output "private_subnet_list" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_list" {
+  value = aws_subnet.database[*].id
+}
